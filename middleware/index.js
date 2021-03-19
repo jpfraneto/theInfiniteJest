1d2308f37b823fd09cbc9e2483b12dcdddff8abd
jpfraneto <jpfraneto@gmail.com>
2021-03-19 08:10:44 -0300

    Fixed the mailing stuff

--- middleware/index.js
+++ middleware/index.js
@@ -92,8 +92,8 @@ middlewareObj.isUser = function(req, res, next){
 }
 
 middlewareObj.sendVerificationEmail = async function (username, userEmail, verificationCode) {
-    const verificationUrl = "http://localhost:3000/verifyEmail/" + verificationCode;
-    // const verificationUrl = "https://www.human-music.com/verifyEmail/" + verificationCode;
+    // const verificationUrl = "http://localhost:3000/verifyEmail/" + verificationCode;
+    const verificationUrl = "https://www.human-music.com/verifyEmail/" + verificationCode;
 
     const transporter = nodemailer.createTransport({
         host: 'mail.privateemail.com',
@@ -125,8 +125,8 @@ middlewareObj.sendVerificationEmail = async function (username, userEmail, verif
 }
 
 middlewareObj.sendResetEmail = async function (username, userEmail, resetCode) {
-    // const verificationUrl = "https://www.human-music.com/password_reset/" + resetCode;
-    const verificationUrl = "http://localhost:3000/password_reset/" + resetCode;
+    const verificationUrl = "https://www.human-music.com/password_reset/" + resetCode;
+    // const verificationUrl = "http://localhost:3000/password_reset/" + resetCode;
     
     const transporter = nodemailer.createTransport({
         host: 'mail.privateemail.com',
